e0392d477ccfeaae6a562fc3cdd4a608be7a1a9b
vijtrip2 <62766875+vijtrip2@users.noreply.github.com>
2021-05-03 14:55:52 -0700

    Push helm chart for stable release.

--- cd/scripts/release-controller.sh
+++ cd/scripts/release-controller.sh
@@ -74,79 +74,83 @@ fi
 
 echo "VERSION is $VERSION"
 
-## Setup the destination repository for docker and helm
-#perform_buildah_and_helm_login
-#
-## Determine parameters for docker-build command
-#pushd "$WORKSPACE_DIR"/"$AWS_SERVICE"-controller 1>/dev/null
-#
-#SERVICE_CONTROLLER_GIT_COMMIT=$(git rev-parse HEAD)
-#QUIET=${QUIET:-"false"}
-#BUILD_DATE=$(date +%Y-%m-%dT%H:%M)
-#CONTROLLER_IMAGE_DOCKERFILE_PATH=$CD_DIR/controller/Dockerfile
-#
-#DEFAULT_DOCKER_REPOSITORY="public.ecr.aws/aws-controllers-k8s/controller"
-#DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-"$DEFAULT_DOCKER_REPOSITORY"}
-#DEFAULT_AWS_SERVICE_DOCKER_IMG_TAG="${AWS_SERVICE}-${VERSION}-do-not-use"
-#AWS_SERVICE_DOCKER_IMG_TAG=${AWS_SERVICE_DOCKER_IMG_TAG:-"$DEFAULT_AWS_SERVICE_DOCKER_IMG_TAG"}
-#AWS_SERVICE_DOCKER_IMG=${AWS_SERVICE_DOCKER_IMG:-"$DOCKER_REPOSITORY:$AWS_SERVICE_DOCKER_IMG_TAG"}
-#DOCKER_BUILD_CONTEXT="$WORKSPACE_DIR"
-#
-#popd 1>/dev/null
-#
-#cd "$WORKSPACE_DIR"
-#
-#if [[ $QUIET = "false" ]]; then
-#    echo "building '$AWS_SERVICE' controller docker image with tag: ${AWS_SERVICE_DOCKER_IMG}"
-#    echo " git commit: $SERVICE_CONTROLLER_GIT_COMMIT"
-#fi
-#
-#echo "AWS_SERVICE_DOCKER_IMG: $AWS_SERVICE_DOCKER_IMG"
-#echo "DOCKERFILE: $CONTROLLER_IMAGE_DOCKERFILE_PATH"
-#echo "AWS_SERVICE: $AWS_SERVICE"
-#echo "SERVICE_CONTROLLER_GIT_VERSION: $VERSION"
-#echo "SERVICE_CONTROLLER_GIT_COMMIT: $SERVICE_CONTROLLER_GIT_COMMIT"
-#echo "DOCKER_BUILD_CONTEXT: $DOCKER_BUILD_CONTEXT"
-#
-#buildah bud \
-#  --quiet="$QUIET" \
-#  -t "$AWS_SERVICE_DOCKER_IMG" \
-#  -f "$CONTROLLER_IMAGE_DOCKERFILE_PATH" \
-#  --build-arg service_alias="$AWS_SERVICE" \
-#  --build-arg service_controller_git_version="$VERSION" \
-#  --build-arg service_controller_git_commit="$SERVICE_CONTROLLER_GIT_COMMIT" \
-#  --build-arg build_date="$BUILD_DATE" \
-#  "${DOCKER_BUILD_CONTEXT}"
-#
-#if [ $? -ne 0 ]; then
-#  exit 2
-#fi
-#
-#echo "Pushing '$AWS_SERVICE' controller image with tag: ${AWS_SERVICE_DOCKER_IMG_TAG}"
-#
-#buildah push "${AWS_SERVICE_DOCKER_IMG}"
-#
-#if [ $? -ne 0 ]; then
-#  exit 2
-#fi
-#
-#DEFAULT_HELM_REGISTRY="public.ecr.aws/aws-controllers-k8s"
-#DEFAULT_HELM_REPO="chart"
-#DEFAULT_RELEASE_VERSION="unknown"
-#
-#HELM_REGISTRY=${HELM_REGISTRY:-$DEFAULT_HELM_REGISTRY}
-#HELM_REPO=${HELM_REPO:-$DEFAULT_HELM_REPO}
-#
-#export HELM_EXPERIMENTAL_OCI=1
-#
-#if [[ -d "$SERVICE_CONTROLLER_DIR/helm" ]]; then
-#    echo -n "Generating Helm chart package for $AWS_SERVICE@$VERSION ... "
-#    helm chart save "$SERVICE_CONTROLLER_DIR"/helm/ "$HELM_REGISTRY/$HELM_REPO:$AWS_SERVICE-$VERSION-do-not-use"
-#    echo "ok."
-#    helm chart push "$HELM_REGISTRY/$HELM_REPO:$AWS_SERVICE-$VERSION-do-not-use"
-#else
-#    echo "Error building Helm packages:" 1>&2
-#    echo "$SERVICE_CONTROLLER_SOURCE_PATH/helm is not a directory." 1>&2
-#    echo "${USAGE}"
-#    exit 1
-#fi
+# Setup the destination repository for docker and helm
+perform_buildah_and_helm_login
+
+# Do not rebuild controller image for stable releases
+if ! (echo "$VERSION" | grep -Eq "stable$"); then
+  # Determine parameters for docker-build command
+  pushd "$WORKSPACE_DIR"/"$AWS_SERVICE"-controller 1>/dev/null
+
+  SERVICE_CONTROLLER_GIT_COMMIT=$(git rev-parse HEAD)
+  QUIET=${QUIET:-"false"}
+  BUILD_DATE=$(date +%Y-%m-%dT%H:%M)
+  CONTROLLER_IMAGE_DOCKERFILE_PATH=$CD_DIR/controller/Dockerfile
+
+  DEFAULT_DOCKER_REPOSITORY="public.ecr.aws/aws-controllers-k8s/controller"
+  DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-"$DEFAULT_DOCKER_REPOSITORY"}
+  DEFAULT_AWS_SERVICE_DOCKER_IMG_TAG="${AWS_SERVICE}-${VERSION}-do-not-use"
+  AWS_SERVICE_DOCKER_IMG_TAG=${AWS_SERVICE_DOCKER_IMG_TAG:-"$DEFAULT_AWS_SERVICE_DOCKER_IMG_TAG"}
+  AWS_SERVICE_DOCKER_IMG=${AWS_SERVICE_DOCKER_IMG:-"$DOCKER_REPOSITORY:$AWS_SERVICE_DOCKER_IMG_TAG"}
+  DOCKER_BUILD_CONTEXT="$WORKSPACE_DIR"
+
+  popd 1>/dev/null
+
+  cd "$WORKSPACE_DIR"
+
+  if [[ $QUIET = "false" ]]; then
+      echo "building '$AWS_SERVICE' controller docker image with tag: ${AWS_SERVICE_DOCKER_IMG}"
+      echo " git commit: $SERVICE_CONTROLLER_GIT_COMMIT"
+  fi
+
+  echo "AWS_SERVICE_DOCKER_IMG: $AWS_SERVICE_DOCKER_IMG"
+  echo "DOCKERFILE: $CONTROLLER_IMAGE_DOCKERFILE_PATH"
+  echo "AWS_SERVICE: $AWS_SERVICE"
+  echo "SERVICE_CONTROLLER_GIT_VERSION: $VERSION"
+  echo "SERVICE_CONTROLLER_GIT_COMMIT: $SERVICE_CONTROLLER_GIT_COMMIT"
+  echo "DOCKER_BUILD_CONTEXT: $DOCKER_BUILD_CONTEXT"
+
+  buildah bud \
+    --quiet="$QUIET" \
+    -t "$AWS_SERVICE_DOCKER_IMG" \
+    -f "$CONTROLLER_IMAGE_DOCKERFILE_PATH" \
+    --build-arg service_alias="$AWS_SERVICE" \
+    --build-arg service_controller_git_version="$VERSION" \
+    --build-arg service_controller_git_commit="$SERVICE_CONTROLLER_GIT_COMMIT" \
+    --build-arg build_date="$BUILD_DATE" \
+    "${DOCKER_BUILD_CONTEXT}"
+
+  if [ $? -ne 0 ]; then
+    exit 2
+  fi
+
+  echo "Pushing '$AWS_SERVICE' controller image with tag: ${AWS_SERVICE_DOCKER_IMG_TAG}"
+
+  buildah push "${AWS_SERVICE_DOCKER_IMG}"
+
+  if [ $? -ne 0 ]; then
+    exit 2
+  fi
+fi
+
+cd "$WORKSPACE_DIR"
+
+DEFAULT_HELM_REGISTRY="public.ecr.aws/aws-controllers-k8s"
+DEFAULT_HELM_REPO="chart"
+
+HELM_REGISTRY=${HELM_REGISTRY:-$DEFAULT_HELM_REGISTRY}
+HELM_REPO=${HELM_REPO:-$DEFAULT_HELM_REPO}
+
+export HELM_EXPERIMENTAL_OCI=1
+
+if [[ -d "$SERVICE_CONTROLLER_DIR/helm" ]]; then
+    echo -n "Generating Helm chart package for $AWS_SERVICE@$VERSION ... "
+    helm chart save "$SERVICE_CONTROLLER_DIR"/helm/ "$HELM_REGISTRY/$HELM_REPO:$AWS_SERVICE-$VERSION-do-not-use"
+    echo "ok."
+    helm chart push "$HELM_REGISTRY/$HELM_REPO:$AWS_SERVICE-$VERSION-do-not-use"
+else
+    echo "Error building Helm packages:" 1>&2
+    echo "$SERVICE_CONTROLLER_SOURCE_PATH/helm is not a directory." 1>&2
+    echo "${USAGE}"
+    exit 1
+fi
