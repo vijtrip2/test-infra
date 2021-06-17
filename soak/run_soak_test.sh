b961ed2942b676362f1da64520f1d2e1866e1320
Vijay Tripathi <vijat@amazon.com>
2021-06-17 22:12:49 +0000

    override durationMinutes from soakconfig.

--- soak/run_soak_test.sh
+++ soak/run_soak_test.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 
 DEFAULT_SOAK_CONFIG_PATH="$(pwd)/default_soak_config.yaml"
 CONTROLLER_SOAK_CONFIG_PATH="$CONTROLLER_E2E_PATH/soak_config.yaml"
+DEFAULT_SOAK_DURATION_MINUTES=${DEFAULT_SOAK_DURATION_MINUTES:-"1440"}
 
 if [[ -f "$CONTROLLER_SOAK_CONFIG_PATH" ]]; then
 	SOAK_CONFIG_PATH=$CONTROLLER_SOAK_CONFIG_PATH
@@ -20,11 +21,13 @@ Usage:
 Environment variables:
   START_TIME_EPOCH_SECONDS:   Start time of soak test run in seconds from epoch.
   CONTROLLER_E2E_PATH:        Path where e2e tests reside for ack service controller.
+  DEFAULT_SOAK_DURATION_MINUTES: Default duration of soak test execution in minutes.
 "
 
+SOAK_DURATION_MINUTES=$(yq eval ".durationMinutes // \"$DEFAULT_SOAK_DURATION_MINUTES\"" $SOAK_CONFIG_PATH) #default: $DEFAULT_SOAK_DURATION_MINUTES
 echo "[INFO] Starting the soak test run."
 echo "[INFO] START_TIME_EPOCH_SECONDS is $START_TIME_EPOCH_SECONDS"
-((END_TIME_EPOCH_SECONDS= "$START_TIME_EPOCH_SECONDS" + "$SOAK_TEST_DURATION_MINUTES"*60))
+((END_TIME_EPOCH_SECONDS= "$START_TIME_EPOCH_SECONDS" + "$SOAK_DURATION_MINUTES"*60))
 echo "[INFO] END_TIME_EPOCH_SECONDS is $END_TIME_EPOCH_SECONDS"
 
 pushd "${CONTROLLER_E2E_PATH}" 1>/dev/null
@@ -39,10 +42,10 @@ do
   echo "[INFO] Current time is $(date +%s%3N) and end time is $END_TIME_EPOCH_SECONDS in epoch seconds. Executing e2e tests..."
   for marker in `echo $ALL_PYTEST_MARKERS`
   do
-	  log_level=$(yq eval ".pytestMarkers.$marker.logLevel // \"info\"" $SOAK_CONFIG_PATH)
-	  num_threads=$(yq eval ".pytestMarkers.$marker.numThreads // \"auto\"" $SOAK_CONFIG_PATH)
-	  dist=$(yq eval ".pytestMarkers.$marker.dist // \"no\"" $SOAK_CONFIG_PATH)
-	  log_cli=$(yq eval ".pytestMarkers.$marker.logCli // \"true\"" $SOAK_CONFIG_PATH)
+	  log_level=$(yq eval ".pytestMarkers.$marker.logLevel // \"info\"" $SOAK_CONFIG_PATH) #default: info
+	  num_threads=$(yq eval ".pytestMarkers.$marker.numThreads // \"auto\"" $SOAK_CONFIG_PATH) #default: auto
+	  dist=$(yq eval ".pytestMarkers.$marker.dist // \"no\"" $SOAK_CONFIG_PATH) # default: no
+	  log_cli=$(yq eval ".pytestMarkers.$marker.logCli // \"true\"" $SOAK_CONFIG_PATH) # default: true
 	  pytest -m $marker -n $num_threads --dist $dist -o log_cli=$log_cli --log-cli-level $log_level --log-level $log_level .
   done
 done
