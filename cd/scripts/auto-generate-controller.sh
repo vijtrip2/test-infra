21ad562b3c9c52b64c72b041c724fe8787283bff
vijtrip2 <62766875+vijtrip2@users.noreply.github.com>
2021-09-02 15:26:46 -0700

    use template files for issues and pr body content.

--- cd/scripts/auto-generate-controller.sh
+++ cd/scripts/auto-generate-controller.sh
@@ -59,13 +59,15 @@ for CONTROLLER_NAME in $CONTROLLER_NAMES; do
   echo "auto-generate-controller.sh][INFO] ACK runtime version for new controller will be $ACK_RUNTIME_VERSION. Current version is $SERVICE_RUNTIME_VERSION."
   echo "auto-generate-controller.sh][INFO] Generating new controller code using command 'make build-controller'."
   export SERVICE=$SERVICE_NAME
-  if ! make build-controller >/dev/null 2>>/tmp/"$SERVICE_NAME"_failure_logs; then
-    cat /tmp/"$SERVICE_NAME"_failure_logs
+  MAKE_BUILD_OUTPUT_FILE=/tmp/"$SERVICE_NAME"_make_build_output
+  MAKE_BUILD_ERROR_FILE=/tmp/"$SERVICE_NAME"_make_build_error
+  if ! make build-controller > "$MAKE_BUILD_OUTPUT_FILE" 2>"$MAKE_BUILD_ERROR_FILE"; then
+    cat "$MAKE_BUILD_ERROR_FILE"
 
-    echo "auto-generate-controller.sh][ERROR] failure while executing 'build-controller' make target. Creating/Updating GitHub issue."
+    echo "auto-generate-controller.sh][ERROR] failure while executing 'make build-controller' command. Creating/Updating GitHub issue."
     ISSUE_TITLE="Errors while generating $CONTROLLER_NAME for ACK runtime $ACK_RUNTIME_VERSION"
 
-    echo -n "auto-generate-controller.sh][INFO] Querying already open GH issue ... "
+    echo -n "auto-generate-controller.sh][INFO] Querying already open GitHub issue ... "
     ISSUE_NUMBER=$(gh issue list -R "$GH_ORG/$GH_ISSUE_REPO" -L 1 -s open --json number -S "$ISSUE_TITLE" --jq '.[0].number' -A @me )
     if [[ $? -ne 0 ]]; then
       echo ""
@@ -73,46 +75,40 @@ for CONTROLLER_NAME in $CONTROLLER_NAMES; do
       continue
     fi
     echo "ok."
-  COMMAND_OUTPUT=$(cat /tmp/"$SERVICE_NAME"_failure_logs)
+
+    MAKE_BUILD_OUTPUT=$(cat "$MAKE_BUILD_OUTPUT_FILE")
+    MAKE_BUILD_ERROR_OUTPUT=$(cat "$MAKE_BUILD_ERROR_FILE")
+    GH_ISSUE_BODY_TEMPLATE_FILE="$THIS_DIR/gh_issue_body_template.txt"
+    GH_ISSUE_BODY_FILE=/tmp/"SERVICE_NAME"_gh_issue_body
+    eval "echo \"$(cat "$GH_ISSUE_BODY_TEMPLATE_FILE")\"" > $GH_ISSUE_BODY_FILE
 
     if [[ -z $ISSUE_NUMBER ]]; then
-      echo -n "auto-generate-controller.sh][INFO] No open issues exist. Creating a new GitHub issue ... "
-      # TODO: move the error string into issue description.
-      if ! gh issue create -R "$GH_ORG/$GH_ISSUE_REPO" -t "$ISSUE_TITLE" -b "$ISSUE_BODY" . See the latest comment for error output." >/dev/null ; then
+      echo -n "auto-generate-controller.sh][INFO] No open issues exist. Creating a new GitHub issue inside $GH_ORG/$GH_ISSUE_REPO ... "
+      if ! gh issue create -R "$GH_ORG/$GH_ISSUE_REPO" -t "$ISSUE_TITLE" -F "$GH_ISSUE_BODY_FILE" >/dev/null ; then
         echo ""
         echo "auto-generate-controller.sh][ERROR] Unable to create GitHub issue for reporting failure. Skipping $CONTROLLER_NAME."
         continue
       fi
       echo "ok"
-
-      echo -n "auto-generate-controller.sh][INFO] Sleeping for 10 seconds ... "
-      sleep 10
-      echo "ok"
-
-      echo -n "auto-generate-controller.sh][INFO] Querying the issue number of newly created GitHub issue ... "
-      ISSUE_NUMBER=$(gh issue list -R "$GH_ORG/$GH_ISSUE_REPO" -L 1 -s open --json number -S "$ISSUE_TITLE" --jq '.[0].number' -A @me )
-      if [[ $? -ne 0 || -z $ISSUE_NUMBER ]]; then
+      continue
+    else
+      echo -n "auto-generate-controller.sh][INFO] Updating error output in the body of existing issue#$ISSUE_NUMBER inside $GH_ORG/$GH_ISSUE_REPO ... "
+      if ! gh issue edit "$ISSUE_NUMBER" -R "$GH_ORG/$GH_ISSUE_REPO" -F "$GH_ISSUE_BODY_FILE" >/dev/null; then
         echo ""
-        echo "auto-generate-controller.sh][ERROR] Unable to query open github issue. Skipping $CONTROLLER_NAME."
+        echo "auto-generate-controller.sh][ERROR] Unable to edit GitHub issue$ISSUE_NUMBER with latest 'make build-controller' error. Skipping $CONTROLLER_NAME."
         continue
       fi
       echo "ok"
-    fi
-
-    echo -n "auto-generate-controller.sh][INFO] Adding error output as comment in issue#$ISSUE_NUMBER in $GH_ISSUE_REPO ... "
-    if ! gh issue comment "$ISSUE_NUMBER" -R "$GH_ORG/$GH_ISSUE_REPO" -F /tmp/"$SERVICE_NAME"_failure_logs >/dev/null; then
-      echo ""
-      echo "auto-generate-controller.sh][ERROR] Unable to add error output as issue comment. Skipping $CONTROLLER_NAME."
       continue
     fi
-    echo "ok"
-
-    # Skip creating PR for this service controller after updating GH issue.
+    # Skip creating PR for this service controller after updating GitHub issue.
     continue
   fi
 
+  # print make build output in prowjob logs
+  cat "$MAKE_BUILD_OUTPUT_FILE"
   pushd "$WORKSPACE_DIR/$CONTROLLER_NAME" >/dev/null
-    echo -n "auto-generate-controller.sh][INFO] Updating go.mod file in $CONTROLLER_NAME ... "
+    echo -n "auto-generate-controller.sh][INFO] Updating 'go.mod' file in $CONTROLLER_NAME ... "
     if ! sed -i "s|aws-controllers-k8s/runtime $SERVICE_RUNTIME_VERSION|aws-controllers-k8s/runtime $ACK_RUNTIME_VERSION|" go.mod >/dev/null; then
       echo ""
       echo "auto-generate-controller.sh][ERROR] Unable to update go.mod file with latest runtime version. Skipping $CONTROLLER_NAME."
@@ -139,7 +135,7 @@ for CONTROLLER_NAME in $CONTROLLER_NAMES; do
     echo "ok"
 
     echo -n "auto-generate-controller.sh][INFO] Pushing changes to branch '$PR_SOURCE_GH_BRANCH' ... "
-    if ! git push --force https://$GITHUB_TOKEN@github.com/vijtrip2/$CONTROLLER_NAME.git $LOCAL_GIT_BRANCH:$PR_SOURCE_GH_BRANCH >/dev/null 2>&1; then
+    if ! git push --force "https://$GITHUB_TOKEN@github.com/vijtrip2/$CONTROLLER_NAME.git" "$LOCAL_GIT_BRANCH:$PR_SOURCE_GH_BRANCH" >/dev/null 2>&1; then
       echo ""
       echo "auto-generate-controller.sh][ERROR] Failed to push the latest changes into remote repository. Skipping $CONTROLLER_NAME."
       continue
@@ -149,27 +145,34 @@ for CONTROLLER_NAME in $CONTROLLER_NAMES; do
     echo -n "auto-generate-controller.sh][INFO] Finding existing open pull requests ... "
     PR_NUMBER=$(gh pr list -R "$GH_ORG/$CONTROLLER_NAME" -A @me -L 1 -s open --json number -S "$COMMIT_MSG" --jq '.[0].number')
     if [[ $? -ne 0 ]]; then
+      echo ""
       echo "auto-generate-controller.sh][ERROR] Failed to query for an existing pull request for $GH_ORG/$CONTROLLER_NAME , from $PR_SOURCE_GH_BRANCH -> $PR_TARGET_GH_BRANCH branch."
-    fi
-    echo "ok"
-
-    if [[ -n $PR_NUMBER ]]; then
-      echo "auto-generate-controller.sh][INFO] PR#$PR_NUMBER already exists for $GH_ORG/$CONTROLLER_NAME , from $PR_SOURCE_GH_BRANCH -> $PR_TARGET_GH_BRANCH branch."
     else
-      echo -n "auto-generate-controller.sh][INFO] No Existing PRs found. Creating a new pull request for $GH_ORG/$CONTROLLER_NAME , from $PR_SOURCE_GH_BRANCH -> $PR_TARGET_GH_BRANCH branch ... "
-      if [[ ! -f /tmp/ack-bot-pr-body ]]; then
-        touch /tmp/ack-bot-pr-body
-        echo "$COMMIT_MSG" >> /tmp/ack-bot-pr-body
-        echo "By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license." >> /tmp/ack-bot-pr-body
-      fi
+      echo "ok"
+    fi
 
-      if ! gh pr create -R "$GH_ORG/$CONTROLLER_NAME" -t "$COMMIT_MSG" -F /tmp/ack-bot-pr-body -H $PR_SOURCE_GH_BRANCH >/dev/null ; then
+    MAKE_BUILD_OUTPUT=$(cat "$MAKE_BUILD_OUTPUT_FILE")
+    GH_PR_BODY_TEMPLATE_FILE="$THIS_DIR/gh_pr_body_template.txt"
+    GH_PR_BODY_FILE=/tmp/"SERVICE_NAME"_gh_pr_body
+    eval "echo \"$(cat "$GH_PR_BODY_TEMPLATE_FILE")\"" > $GH_PR_BODY_FILE
+
+    if [[ -z $PR_NUMBER ]]; then
+      echo -n "auto-generate-controller.sh][INFO] No Existing PRs found. Creating a new pull request for $GH_ORG/$CONTROLLER_NAME , from $PR_SOURCE_GH_BRANCH -> $PR_TARGET_GH_BRANCH branch ... "
+      if ! gh pr create -R "$GH_ORG/$CONTROLLER_NAME" -t "$COMMIT_MSG" -F "$GH_PR_BODY_FILE" -H "$PR_SOURCE_GH_BRANCH" -B "$PR_TARGET_GH_BRANCH" >/dev/null ; then
         echo ""
         echo "auto-generate-controller.sh][ERROR] Failed to create pull request. Skipping $CONTROLLER_NAME."
         continue
-      else
-        echo "ok"
       fi
+      echo "ok"
+    else
+      echo "auto-generate-controller.sh][INFO] PR#$PR_NUMBER already exists for $GH_ORG/$CONTROLLER_NAME , from $PR_SOURCE_GH_BRANCH -> $PR_TARGET_GH_BRANCH branch."
+      echo -n "auto-generate-controller.sh][INFO] Updating PR body with latest 'make build-controller' output..."
+      if ! gh pr edit "$PR_NUMBER" -R "$GH_ORG/$CONTROLLER_NAME" -F "$GH_PR_BODY_FILE" >/dev/null ; then
+        echo ""
+        echo "auto-generate-controller.sh][ERROR] Failed to update pull request."
+        continue
+      fi
+      echo "ok"
     fi
     echo "auto-generate-controller.sh][INFO] Done. :) "
   popd >/dev/null
